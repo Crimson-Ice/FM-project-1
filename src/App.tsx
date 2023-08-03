cfa56a369d5c5a8349e19f834457fc9c3d909056
CrimsonLand <totonindu60320@gmail.com>
2023-08-03 18:44:57 +0200

    ajut for large screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Summary from "./Components/Summary";
 function App() {
     return (
         <div className="flex h-full w-full items-center justify-center">
-            <div className="flex h-full w-full flex-col rounded-2xl bg-primary-white xs:h-3/4 xs:w-2/4 xs:min-w-[800px] xs:flex-row xs:shadow-xl">
+            <div className="flex h-full w-full flex-col rounded-2xl bg-primary-white xs:h-3/4 xs:max-h-[700px] xs:w-2/4 xs:min-w-[800px] xs:max-w-[800px] xs:flex-row xs:shadow-xl">
                 <Result />
                 <Summary />
             </div>
